a4a0d320ef0eb5f326e7ffe37ceefbbdbe8205a2
junhr99 <junhr99@naver.com>
2021-11-12 15:09:23 +0900

    Refactoring Folder Structure

--- components/main/Total.js
+++ components/main/Total.js
@@ -1,57 +1,45 @@
-<<<<<<< HEAD
-import React, { useState } from "react";
-import { View, Image, Dimensions } from "react-native";
-import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
-export default function Total() {
-=======
 import { NavigationContainer } from "@react-navigation/native";
 import React, { useState } from "react";
 import { View, Image, Dimensions } from "react-native";
 import { FlatList, TouchableOpacity } from "react-native-gesture-handler";
 export default function Total(props) {
->>>>>>> e63e6dcbadf0d42de99bad5ebf3203bb3ff62420
   const [data, setData] = useState([
     {
       name: "banana",
       id: "1",
       source: require("./img/1.png"),
+      //uri: "./img/1.png",
     },
     {
       name: "china",
       id: "2",
       source: require("./img/2.png"),
+      //uri: "./img/2.png",
     },
     {
       name: "halloween",
       id: "3",
       source: require("./img/5.png"),
+      //uri: "./img/5.png",
     },
     {
       name: "mawin",
       id: "4",
       source: require("./img/4.png"),
+      //uri: "./img/4.png",
     },
     {
       name: "sunglasses",
       id: "5",
       source: require("./img/6.png"),
+      //uri: "./img/6.png",
     },
     // and so on...
   ]);
-  console.log(data[1])
+  console.log(data[1]);
   return (
     <FlatList
       data={data}
-<<<<<<< HEAD
-      renderItem={({ item }) => (
-        <TouchableOpacity>
-          <Image
-            style={{ width: Dimensions.get("window").width / 3 }}
-            source={item.source}
-          />
-        </TouchableOpacity>
-      )}
-=======
       renderItem={({ item }) => {
         return (
           <TouchableOpacity
@@ -65,13 +53,15 @@ export default function Total(props) {
             }}
           >
             <Image
-              style={{ width: Dimensions.get("window").width / 3 }}
+              style={{
+                width: Dimensions.get("window").width / 3,
+                height: Dimensions.get("window").width / 3,
+              }}
               source={item.source}
             />
           </TouchableOpacity>
         );
       }}
->>>>>>> e63e6dcbadf0d42de99bad5ebf3203bb3ff62420
       keyExtractor={(item) => String(item.id)}
       numColumns={3}
     />
--- components/main/TotalList.js
+++ components/main/TotalList.js
@@ -7,19 +7,36 @@ export default function TotalList(props) {
     props.route.params.item.slice(startIndex - 1, endIndex)
   );
 
+  Image.getSize(data[0].uri, (width, height) => {
+    console.log("w,h :", width, height);
+  });
+  console.log("Sss:", Image.resolveAssetSource(data[0].source).width);
+
   console.log(data);
   return (
     <FlatList
       data={data}
       renderItem={({ item }) => (
         <View
-          style={{ borderWidth: 5, borderColor: "skyblue", marginBottom: 10 }}
+          style={{
+            borderWidth: 5,
+            borderColor: "skyblue",
+            marginBottom: 10,
+          }}
         >
           <Text>이름</Text>
           <Text>날짜</Text>
           <Image
-            style={{ width: Dimensions.get("window").width }}
+            style={{
+              width: Image.resolveAssetSource(item.source).width,
+              height: Image.resolveAssetSource(item.source).height,
+              /*
+              width: Dimensions.get("window").width,
+              height: Dimensions.get("window").height / 3,
+              */
+            }}
             source={item.source}
+            resizeMode="contain"
           />
           <Text>{(item.name, item.id)}</Text>
           <Button title="신고하기"></Button>
