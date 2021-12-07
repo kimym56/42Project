62f250cdd5b74e72667971afb501a6dbe95fdec3
kimym56 <74525390+kimym56@users.noreply.github.com>
2021-12-07 22:46:32 +0900

    Expand the touch area in Monthly; Separate days from matrix

--- src/components/Buttons/NextCalendarButton/style.js
+++ src/components/Buttons/NextCalendarButton/style.js
@@ -14,6 +14,6 @@ const styles = StyleSheet.create({
   },
 });
 
-
+//hello
 
 export default styles;
--- src/pages/Calendar/Monthly/Monthly.js
+++ src/pages/Calendar/Monthly/Monthly.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, Touchable } from "react-native";
 
 import PrevCalendarButton from "components/Buttons/PrevCalendarButton";
 import NextCalendarButton from "components/Buttons/NextCalendarButton";
 import DateCalendarText from "components/Texts/DateCalendarText"
+import Calendar from "..";
 export class Monthly extends Component {
 
   weekDays = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -16,7 +17,7 @@ export class Monthly extends Component {
   };
   generateMatrix() {
     var matrix = [];
-    matrix[0] = this.weekDays;
+    //matrix[0] = this.weekDays;
 
     var year = this.state.selectDate.getFullYear();
     var month = this.state.selectDate.getMonth();
@@ -31,6 +32,9 @@ export class Monthly extends Component {
     var counter = 1;
     for (var row = 1; row < 7; row++) {
       matrix[row] = [];
+      if (counter>maxDays){
+        break;
+      }
       for (var col = 0; col < 7; col++) {
         matrix[row][col] = -1;
         if (row == 1 && col >= firstDay) {
@@ -40,8 +44,11 @@ export class Monthly extends Component {
           // Fill in rows only if the counter's not greater than
           // the number of days in the month
           matrix[row][col] = counter++;
+          
         }
+        
       }
+      
     }
     return matrix;
   }
@@ -62,15 +69,42 @@ export class Monthly extends Component {
   render() {
     console.log("monthly render");
     var matrix = this.generateMatrix();
+    var dayMatrix=this.weekDays;
+var days=[];
+days = dayMatrix.map((item, rowIndex) => {
+  return (
+    <View
+      style={{
+        flex: 0.5,
+        flexDirection: "row",
+        padding: 0,
+        justifyContent: "space-around",
+        alignItems: "center",
+        backgroundColor: "white" 
+      }}
+    >
+      <Text
+        style={{
+          flex: 1,
+          height: 20,
+          textAlign: "center",
+          color: rowIndex == 0 ? "#f00" : rowIndex == 6 ? "#00f" : "",
+        }}
+      >
+        {item}
+      </Text>
+    </View>
+  );
+});
 
     var rows = [];
     rows = matrix.map((row, rowIndex) => {
       var rowItems = row.map((item, colIndex) => {
-        return (
+        return (<TouchableOpacity style={{flex:1,borderWidth:1,
+        flexDirection: "row"}}>
           <Text
             style={{
               flex: 1,
-              height: 18,
               textAlign: "center",
               // Highlight header
               backgroundColor: rowIndex == 0 ? "#ddd" : "#fff",
@@ -93,13 +127,15 @@ export class Monthly extends Component {
                   ? "bold"
                   : "",
             }}
-
+            
             onPress={() => {
-              this._onPress2(item);
+              if (item!=-1){
+              this._onPress2(item);}
             }}
           >
             {item != -1 ? item : ""}
           </Text>
+          </TouchableOpacity>
         );
       });
       return (
@@ -107,7 +143,7 @@ export class Monthly extends Component {
           style={{
             flex: 1,
             flexDirection: "row",
-            padding: 15,
+            padding: 0,
             justifyContent: "space-around",
           }}
         >
@@ -122,8 +158,21 @@ export class Monthly extends Component {
           <PrevCalendarButton onPress={() => this.changeMonth(-1)} />
           <DateCalendarText isMWD = "M" selectDate={this.state.selectDate} />
           <NextCalendarButton onPress={() => this.changeMonth(+1)} />
+      
         </View>
-        <View style={{flex:11}}>{rows}</View>
+        <View
+          style={{
+            flex: 0.5,
+            flexDirection: "row",
+            alignItmes: "center",
+            justifyContent: "center",
+            //marginLeft: (width / 7.6) * 0.6,
+          }}
+        >
+          {days}
+        </View>
+        <View style={{flex:11,backgroundColor:'white'}}>{rows}</View>
+        
       </View>
     );
   }
