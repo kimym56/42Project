033f59037751f6eb434c6d3a9fb46f2abaebef3d
junhr99 <junhr99@naver.com>
2021-12-29 11:51:25 +0900

    Adjust Prev, Next, DateCalendarText to relative position & center alignment

--- src/components/Buttons/NextCalendarButton/style.js
+++ src/components/Buttons/NextCalendarButton/style.js
@@ -5,15 +5,15 @@ const styles = StyleSheet.create({
   container: {
     //flex: 1,
     //backgroundColor: "blue",
-
-    width: 50,
-    height: 36,
+    width: 60,
+    //height: 60,
     backgroundColor: "#236db3",
     overflow: "visible",
     borderRadius: 10,
     marginRight: 20,
     marginLeft: 5,
     justifyContent: "center",
+    display: "flex",
   },
   text: {
     color: "#fff",
