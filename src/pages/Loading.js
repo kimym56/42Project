708e0d65e3681ebecec3d8fd6c3e5925f6ecbea9
kimym56 <74525390+kimym56@users.noreply.github.com>
2022-01-05 17:39:47 +0900

    Add gitignore

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -5,10 +5,17 @@ import { render } from "react-dom";
 export default class Loading extends PureComponent {
   render() {
     return (
-      <ImageBackground
-        source={require("../assets/images/logo.jpg")}
-        style={{ width: "100%", height: "100%" }}
-      />
+      <View style={{  justifyContent: "center",
+      alignItems: "center",
+      height: "100%",backgroundColor:'white'}}>
+        <Image
+          source={require("../assets/images/logo3.png")}
+          style={{
+            width: "100%",
+            height: 100, alignSelf: "center"
+          }}
+        />
+      </View>
     );
   }
 }
