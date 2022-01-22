177bcf961b817b52cb8d1e4c2974eec403ec83f0
junhr99 <junhr99@naver.com>
2022-01-22 23:24:40 +0900

    Change DateCalendarText format in Weekly

--- src/components/Texts/DateCalendarText/index.js
+++ src/components/Texts/DateCalendarText/index.js
@@ -28,6 +28,67 @@ export default function index(props) {
     "November",
     "December",
   ];
+  let firstDate = -1;
+  let lastDate = -1;
+  let week = -1;
+  const changeWeeklyDateFormat = () => {
+    console.log(weeklyDate.getDate());
+    console.log("getDay", weeklyDate.getDay());
+    let tempDate, tempDate2;
+    console.log(weeklyDate.getMonth(), weeklyDate.getDate());
+    tempDate = new Date(weeklyDate);
+    tempDate2 = new Date(weeklyDate);
+
+    if (weeklyDate.getDay() == 0) {
+      firstDate = new Date(tempDate.setDate(weeklyDate.getDate()));
+      firstDate.setMonth(tempDate.getMonth());
+      lastDate = new Date(tempDate2.setDate(weeklyDate.getDate() + 6));
+      lastDate.setMonth(tempDate2.getMonth());
+    } else if (weeklyDate.getDay() == 1) {
+      firstDate = new Date(tempDate.setDate(weeklyDate.getDate() - 1));
+      firstDate.setMonth(tempDate.getMonth());
+      lastDate = new Date(tempDate2.setDate(weeklyDate.getDate() + 5));
+      lastDate.setMonth(tempDate2.getMonth());
+    } else if (weeklyDate.getDay() == 2) {
+      firstDate = new Date(tempDate.setDate(weeklyDate.getDate() - 2));
+      firstDate.setMonth(tempDate.getMonth());
+      lastDate = new Date(tempDate2.setDate(weeklyDate.getDate() + 4));
+      lastDate.setMonth(tempDate2.getMonth());
+    } else if (weeklyDate.getDay() == 3) {
+      firstDate = new Date(tempDate.setDate(weeklyDate.getDate() - 3));
+      firstDate.setMonth(tempDate.getMonth());
+      lastDate = new Date(tempDate2.setDate(weeklyDate.getDate() + 3));
+      lastDate.setMonth(tempDate2.getMonth());
+    } else if (weeklyDate.getDay() == 4) {
+      firstDate = new Date(tempDate.setDate(weeklyDate.getDate() - 4));
+      firstDate.setMonth(tempDate.getMonth());
+      lastDate = new Date(tempDate2.setDate(weeklyDate.getDate() + 2));
+      lastDate.setMonth(tempDate2.getMonth());
+    } else if (weeklyDate.getDay() == 5) {
+      firstDate = new Date(tempDate.setDate(weeklyDate.getDate() - 5));
+      firstDate.setMonth(tempDate.getMonth());
+      lastDate = new Date(tempDate2.setDate(weeklyDate.getDate() + 1));
+      lastDate.setMonth(tempDate2.getMonth());
+    } else if (weeklyDate.getDay() == 6) {
+      firstDate = new Date(tempDate.setDate(weeklyDate.getDate() - 6));
+      firstDate.setMonth(tempDate.getMonth());
+      lastDate = new Date(tempDate2.setDate(weeklyDate.getDate()));
+      lastDate.setMonth(tempDate2.getMonth());
+
+      console.log("first", firstDate.getMonth(), firstDate.getDate());
+      console.log("last", lastDate.getMonth(), lastDate.getDate());
+      console.log("sssss", weeklyDate.getDate());
+    }
+
+    /*
+    if (Math.floor(firstDate / 7) != 0) {
+      week = Math.floor(firstDate / 7) + 2;
+    } else {
+      week = firstDate;
+    }
+    */
+    //console.log("week", week);
+  };
   switch (props.isMWD) {
     case "M":
       return (
@@ -46,6 +107,7 @@ export default function index(props) {
         </View>
       );
     case "W":
+      changeWeeklyDateFormat();
       return (
         <View
           style={{
@@ -56,9 +118,8 @@ export default function index(props) {
           }}
         >
           <Text style={styles.text}>
-            {weeklyDate.getMonth() + 1} &nbsp;
-            {weeklyDate.getDate()} &nbsp;
-            {weekDays[weeklyDate.getDay()]}
+            {firstDate.getMonth() + 1 + "월 " + firstDate.getDate() + "일 ~ "}
+            {lastDate.getMonth() + 1 + "월 " + lastDate.getDate() + "일"}
           </Text>
         </View>
       );
